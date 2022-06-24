0af40aafd5a683c481d71b277205e9038c02bf1b
jhonatan Esteferson <Jhonatanjhonatan907@gmail.com>
2022-06-24 18:04:27 -0300

    segundo commit

--- perfil/scripts.js
+++ perfil/scripts.js
@@ -1,9 +1,11 @@
 const inputElement = document.querySelector('.new-task-input');
 const addTaskButton = document.querySelector('.new-task-button');
 
-const taskItemConteiner = document.querySelector('.tasks-conteiner')
+const tasksContainer = document.querySelector('.tasks-container')
 
-const valedateInput = () => inputElement.value.trim().length > 0;
+
+
+const validateInput = () => inputElement.value.trim().length > 0;
 
 const hendleAddTask = () =>{
     const inputIsValid = validateInput();
@@ -16,19 +18,67 @@ const hendleAddTask = () =>{
     const taskItemConteiner = document.createElement('div');
     taskItemConteiner.classList.add('task-item');
 
-    const taskContent = document.createComment('p');
+    const taskContent = document.createElement('p');
     taskContent.innerText = inputElement.value;
+
+    taskContent.addEventListener('click', () => handleClick(taskContent))
+
 
     const deleteItem = document.createElement('i');
     deleteItem.classList.add('far');
-    deleteItem.classList.add('far fa-trash-alt');
+    deleteItem.classList.add('fa-trash-alt');
+
+
+    deleteItem.addEventListener('click', () =>
+     handleDeleteClick(taskItemConteiner,taskContent)
+    );
+    
 
     taskItemConteiner.appendChild(taskContent);
     taskItemConteiner.appendChild(deleteItem);
 
-    tasksConteiner.appendChild(taskItemConteiner);
+    tasksContainer.appendChild(taskItemConteiner);
+
+    inputElement.value = "";
+
+
+    updateLocalStorage();
+
+
 
 };
+
+
+const handleClick = (taskContent) =>{
+    const tasks = tasksContainer.childNodes;
+    
+
+    for (const task of tasks){
+        const currentTaskIsBeingClicked = task.firstChild.isSameNode(taskContent)
+
+        if (currentTaskIsBeingClicked){
+            task.firstChild.classList.toggle("completed");
+        }
+    }
+    updateLocalStorage();
+};
+
+
+
+const handleDeleteClick = (taskItemConteiner,taskContent) =>{
+    const tasks = tasksContainer.childNodes;
+
+    for (const task of tasks){
+        const currentTaskIsBeingClicked = task.firstChild.isSameNode(taskContent)
+
+        if (currentTaskIsBeingClicked){
+            taskItemConteiner.remove();
+        }
+    }
+    updateLocalStorage();
+};
+
+
 
 const hendleInputChange = () => {
     const inputIsValid = validateInput();
@@ -38,6 +88,73 @@ const hendleInputChange = () => {
     }
 };
 
+
+const updateLocalStorage = () =>{
+    const tasks = tasksContainer.childNodes;
+
+   
+
+    const localStorageTasks = [... tasks].map(task =>{
+        const content = task.firstChild;
+        const isCompleted = content.classList.contains('completed')
+
+        return {description: content.innerText, isCompleted: isCompleted};
+    });
+
+
+    localStorage.setItem('tasks', JSON.stringify(localStorageTasks));
+
+};
+
+
+const refreshTasksUsingLocalStorege = () =>{
+    const tasksFromLocalStorege = JSON.parse(localStorage.getItem('tasks'));
+
+    for (const task of tasksFromLocalStorege){
+        const taskItemConteiner = document.createElement('div');
+        taskItemConteiner.classList.add('task-item');
+    
+        const taskContent = document.createElement('p');
+        taskContent.innerText = tesk.description;
+
+        if (task.isCompleted){
+            taskContent.classList.add("completed");
+
+        }
+
+
+
+    
+        taskContent.addEventListener('click', () => handleClick(taskContent))
+    
+    
+        const deleteItem = document.createElement('i');
+        deleteItem.classList.add('far');
+        deleteItem.classList.add('fa-trash-alt');
+    
+    
+        deleteItem.addEventListener('click', () =>
+         handleDeleteClick(taskItemConteiner,taskContent)
+        );
+        
+    
+        taskItemConteiner.appendChild(taskContent);
+        taskItemConteiner.appendChild(deleteItem);
+    
+        tasksContainer.appendChild(taskItemConteiner);
+
+    }
+
+};
+
+
+
+
+
+
+
+refreshTasksUsingLocalStorege();
+
 addTaskButton.addEventListener('click', () => hendleAddTask());
 
 inputElement.addEventListener('change', () => hendleInputChange());
@@ -46,3 +163,6 @@ inputElement.addEventListener('change', () => hendleInputChange());
 
 
 console.log('teste')
+
+
+
