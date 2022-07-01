7f0d3ac2cc7286ce3858a1406c371e3a04e91210
Christofer Mendes <christoferluizdsm@gmail.com>
2022-07-01 11:33:40 -0300

    Localhost and files

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const port = "3000";
 
 app.use(express.static("public"))
 
@@ -7,46 +8,7 @@ app.route("/index.html").get((req, res) => res.sendFile(__dirname + "/public/HTM
 
 app.route("/home.html").get((req, res) => res.sendFile(__dirname + "/public/HTML/home.html"))
 
-// app.get("/", function(req, res){
-//   res.sendFile(__dirname + "/public/HTML/index.html")
-// });
-
-
-// class tabs {
-//   constructor(file){
-//     this._file = file;
-//   }
-
-
-//   get GetArea(){
-//     return app.route(`/${this._file}`).get( (req, res) => res.sendFile(__dirname + `/public/HTML/${this._file}`))
-//   }
-// }
-
-// let pages = [
-//   {id: 1, file: new tabs(`index.html`)}
-// ]
-
-
-
-
-
-
-// function tabs (){
-//   const login = app.route("/").get( (req, res) => res.sendFile(__dirname + "/public/HTML/index.html"));
-//   const home = app.route("/home.html").get( (req, res) => res.sendFile(__dirname + "/public/HTML/home.html"));
-  
-// }
-
-// tabs();
-
-
-// app.get("/home.html", function(req, res){
-//   res.sendFile(__dirname + "/public/HTML/home.html")
-// });
-
-
-app.listen("3000", ()=>{
-  console.log("Running server at 3000")
+app.listen(port, ()=>{
+  console.log(`Running at ${port}`)
 })
 
